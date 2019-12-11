e8615861292b9c93573247c671f5a72e7dddebba
Student <example@example.com>
2019-12-10 16:24:45 -0800

    add specs, tests, and methods.

--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -3,6 +3,21 @@ require('ruby_logic')
 
 describe('#ruby_logic') do
   it("turns numbers 1-9 into words") do
-    expect(num_words("1")).to(eq("one"))
+    ones = Number_to_words.new(1)
+    expect(ones.get_ones()).to(eq("one"))
+  end
+end
+
+describe('#ruby_logic') do
+  it("turns numbers 10-19 into words") do
+    teens = Number_to_words.new(13)
+    expect(teens.get_teens()).to(eq("thirteen"))
+  end
+end
+
+describe('#ruby_logic') do
+  it("turns numbers in the tenths place into words") do
+    tenths = Number_to_words.new(20)
+    expect(tenths.get_tenths()).to(eq("twenty"))
   end
 end
